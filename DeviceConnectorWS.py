a087105c2b9985e1dd83d6e095017b927797f1fd
letiziabergamasco <32203891+letiziabergamasco@users.noreply.github.com>
2020-03-26 19:47:53 +0100

    Add files via upload

--- DeviceConnectorWS.py
+++ DeviceConnectorWS.py
@@ -2,7 +2,8 @@ from DeviceConnector import *
 import cherrypy
 import json
 import socket
-        
+import paho.mqtt.client as PahoMQTT       
+import threading
 
 class DeviceConnectorREST(object):
     
@@ -92,7 +93,96 @@ class DeviceConnectorREST(object):
         pass
         return
 
+
+class DeviceConnectorMQTT:
+	
+	def __init__(self, clientID, broker, port, deviceConnector):
+		
+		self.broker = broker
+		self.port = port
+		self.clientID = clientID
+
+		self._topic = ""
+		self._isSubscriber = False
+
+		# create an instance of paho.mqtt.client
+		self._paho_mqtt = PahoMQTT.Client(clientID, False) 
+
+		# register the callback
+		self._paho_mqtt.on_connect = self.myOnConnect
+		self._paho_mqtt.on_message = self.myOnMessageReceived
+
+
+	def myOnConnect (self, paho_mqtt, userdata, flags, rc):
+		print ("Connected to %s with result code: %d" % (self.broker, rc))
+
+	def myOnMessageReceived (self, paho_mqtt , userdata, msg):
+		# A new message is received
+		#self.notifier.notify (msg.topic, msg.payload)
+		print("Message is:")
+		print(msg.payload)
+		print("Topic is:")
+		print(msg.topic)
+
+
+	def myPublish (self, topic, msg):
+		# if needed, you can do some computation or error-check before publishing
+		print ("publishing '%s' with topic '%s'" % (msg, topic))
+		# publish a message with a certain topic
+		self._paho_mqtt.publish(topic, msg, 2)
+
+	def mySubscribe (self, topic):
+		# if needed, you can do some computation or error-check before subscribing
+		print ("subscribing to %s" % (topic))
+		# subscribe for a topic
+		self._paho_mqtt.subscribe(topic, 2)
+
+		# just to remember that it works also as a subscriber
+		self._isSubscriber = True
+		self._topic = topic
+
+	def start(self):
+		#manage connection to broker
+		self._paho_mqtt.connect(self.broker , self.port)
+		self._paho_mqtt.loop_start()
+
+	def stop (self):
+		if (self._isSubscriber):
+			# remember to unsuscribe if it is working also as subscriber 
+			self._paho_mqtt.unsubscribe(self._topic)
+
+		self._paho_mqtt.loop_stop()
+		self._paho_mqtt.disconnect()
+		
+class TemperatureThread(threading.Thread):
+        
+        def __init__(self, deviceConnector, deviceConnectorMQTT, userID):
+            threading.Thread.__init__(self)
+        
+        def run(self):
+            while True:
+                Tsenml = deviceConnector.get_temperature()
+                msg = json.dumps(Tsenml)
+                topic = "MyGreenFridge/"+str(userID)+"/temperature"
+                deviceConnectorMQTT.myPublish(topic, msg)
+                time.sleep(15)
+
+class HumidityThread(threading.Thread):
+        
+        def __init__(self, deviceConnector, deviceConnectorMQTT, userID):
+            threading.Thread.__init__(self)
+        
+        def run(self):
+            while True:
+                Hsenml = deviceConnector.get_humidity()
+                msg = json.dumps(Hsenml)
+                topic = "MyGreenFridge/"+str(userID)+"/humidity"
+                deviceConnectorMQTT.myPublish(topic, msg)
+                time.sleep(15)
+
+
 if __name__ == '__main__':
+    
     
     # standard configuration to serve the url "localhost:8080"
     conf = {
@@ -107,8 +197,24 @@ if __name__ == '__main__':
     s.connect(("8.8.8.8", 80))
     ip = s.getsockname()[0]
     
+    # PROVA --> poi sistemiamo da dove vengono questi parametri
+    userID = "pippo"
+    clientID = "ciccio"
+    broker = ip
+    port = 1883
+    
+    
     # instantiate a DeviceConnector object
     deviceConnector = DeviceConnector(ip)
+    
+    deviceConnectorMQTT = DeviceConnectorMQTT(clientID, broker, port, deviceConnector)
+    deviceConnectorMQTT.start()
+    
+    tempThread = TemperatureThread(deviceConnector, deviceConnectorMQTT, userID)
+    humThread = HumidityThread(deviceConnector, deviceConnectorMQTT, userID)
+    
+    tempThread.start()
+    humThread.start()
     
     # deploy the DeviceConnectorREST class and start the web server
     cherrypy.tree.mount(DeviceConnectorREST(deviceConnector), '/', conf)
