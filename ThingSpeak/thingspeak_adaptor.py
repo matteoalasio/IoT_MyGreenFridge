9ab5dd181ac59d0025d9efa19f70ee9b5a55bb77
Piera Riccio <48887623+PieraRiccio@users.noreply.github.com>
2020-04-15 19:59:26 +0200

    REST working

--- ThingSpeak/thingspeak_adaptor.py
+++ ThingSpeak/thingspeak_adaptor.py
@@ -1,24 +1,24 @@
-# The purpose of this script is to subscribe to topics, 
-# receiving data from the sensors in MQTT and then 
+# The purpose of this script is to subscribe to topics,
+# receiving data from the sensors in MQTT and then
 # publishing them on ThingSpeak channels via HTTP request
 
 ## ---> check after if all these libraries are needed
 #import thingspeak
-#import threading
 import json
-import paho.mqtt.client as PahoMQTT
+#import paho.mqtt.client as PahoMQTT
 import datetime
 import time
 import requests
 import sys
 import urllib
+import cherrypy
 
 ## ---> subscriber class, to manage the messages in MQTT
 class ThingSpeakDataManager :
 
     def __init__(self, userID, broker, port, fridges):
-        """ 
-        This is the constructor function of the class. 
+        """
+        This is the constructor function of the class.
         -----
         userID : identification of a device to which subscribing
               string
@@ -30,7 +30,7 @@ class ThingSpeakDataManager :
         self.userID = userID
         self.broker = broker
         self.port = port
-        self.topic = "" 
+        self.topic = ""
         self._isSubscriber = False
         self._paho_mqtt = PahoMQTT.Client(userID, False)
         self._paho_mqtt.on_connect = self.myOnConnect
@@ -39,7 +39,7 @@ class ThingSpeakDataManager :
 
     def mySubscribe (self, topic):
         """
-        This function allows the subscription to a specific topic, 
+        This function allows the subscription to a specific topic,
         passed as parameters. It is a string.
         """
         # Maybe implement some error-check before subscribing ?
@@ -47,7 +47,7 @@ class ThingSpeakDataManager :
         self._isSubscriber = True
         self._paho_mqtt.subscribe(topic, 2)
         self._topic = topic
-    
+
     def myOnConnect (self, paho_mqtt, userdata):
         """
         This manages the opening of a connection.
@@ -62,11 +62,11 @@ class ThingSpeakDataManager :
 
         """
         print ("Connected to %s with result code: %d" % (self.broker, rc))
-    
+
     def myOnMessageReceived (self, paho_mqtt , userdata, msg):
         # here I must still understand precisely which topics ThingSpeak wants
         """
-        This function manages the recption of a message. 
+        This function manages the recption of a message.
         -----
         paho_mqtt : ????
         userdata: ??????
@@ -74,7 +74,7 @@ class ThingSpeakDataManager :
            JSON?
         """
         # print ("!")
-        
+
         # capire bene cosa fare di TControl e di HControl
         # I am assuming the topic is correct. Maybe I should check this somewhere.
 
@@ -90,7 +90,7 @@ class ThingSpeakDataManager :
                 # CANALI DIVERSI CON UN SOLO FIELD? O STESSO CANALE E VARI FIELD?
                 data = urllib.urlopen("https://api.thingspeak.com/update?api_key="+fridgeAPI+"&field1="+str(value))
                 print ("Temperature value updated on ThingSpeak")
-                # Where do I take the value? 
+                # Where do I take the value?
                 print (value)
 
             elif(msg.topic == '/MyGreenFridge/'+ self.userID + "/" + self.fridgeID +"/humidity"):
@@ -114,7 +114,7 @@ class ThingSpeakDataManager :
 
         self._paho_mqtt.loop_stop()
         self._paho_mqtt.disconnect()
-    
+
 if __name__ == "__main__":
 
     conf = {
@@ -147,7 +147,7 @@ if __name__ == "__main__":
 
     print("Catalog IP is: " + catalogIP)
     print("Catalog port is " + catalogPort)
-    
+
     # retrieve the broker IP and the broker port from the Catalog
     catalogURL = "http://" + catalogIP + ":" + catalogPort
     try:
@@ -155,23 +155,30 @@ if __name__ == "__main__":
         broker = r.json()
         brokerIP = broker["broker_IP"]
         brokerPort = broker["broker_port"]
+        print("Broker IP is: " + str(brokerIP))
+        print("Broker port is: " + str(brokerPort))
     except requests.RequestException as err:
-        #sys.exit("ERROR: cannot retrieve the Broker IP from the Catalog.")
-        pass
-    
+        sys.exit("ERROR: cannot retrieve the Broker IP from the Catalog.")
+
+    print ("This is the list of fridges:")
+    print (fridges)
+    n_fridges = len(fridges)
+    print ("It contains a number of fridges equal to: " + str(n_fridges))
     for f in fridges:
         fridgeID = f["fridgeID"]
         fridgeAPI = f["API"]
-        catalogURL = catalogURL+"?Fridge_ID="+fridgeID
+        print(fridgeID)
+        print(fridgeAPI)
+        catalogURL = catalogURL+"/wasted?Fridge_ID="+fridgeID
         # Taking the wasted products here
         try:
-            r2 = requests.get(catalogURL+ "/wasted")
+            r2 = requests.get(catalogURL)
             wasted_json = r2.json()
+            value = len(wasted_json["Wasted_products"])
             data = urllib.urlopen("https://api.thingspeak.com/update?api_key="+fridgeAPI+"&field3="+str(value))
             print ("Wasted products updated on ThingSpeak")
         except requests.RequestException as err:
-            #did not find the list of wasted products
-            pass
+            sys.exit("ERROR: did not find the list of wasted products")
 
     tsdm = ThingSpeakDataManager(deviceID, brokerip, brokerport, fridges)
 
