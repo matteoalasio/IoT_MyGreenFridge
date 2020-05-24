11a3a0ba05bc6ec760edc86e633cfcfd3a1fe665
Piera Riccio <48887623+PieraRiccio@users.noreply.github.com>
2020-05-24 15:25:28 +0200

    up freeboard

---
+++ freeboard/FreeBoard.py
@@ -0,0 +1,52 @@
+import cherrypy
+import json
+import os, os.path
+
+
+class FreeBoard:
+
+	exposed = True
+
+	def GET(self):
+		index_file = open("index.html")
+		return index_file.read()
+
+	def POST(self, *uri, **params):
+		json_string = params['json_string']
+		file = open("dashboard/dashboard.json", 'w')
+		file.write(json_string)
+
+if __name__ == '__main__':
+	conf = {
+		'/': {
+		'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+		'tools.sessions.on': True,
+		'tools.staticdir.root': os.path.abspath(os.getcwd())
+	},
+	'/static': {
+		'tools.staticdir.on': True,
+		'tools.staticdir.dir': '.'
+	},
+	'/img': {
+		'tools.staticdir.on': True,
+		'tools.staticdir.dir': './img'
+	},
+	'/plugins': {
+		'tools.staticdir.on': True,
+		'tools.staticdir.dir': './plugins'
+	},
+	'/css': {
+		'tools.staticdir.on': True,
+		'tools.staticdir.dir': './css'
+	},
+	'/js': {
+		'tools.staticdir.on': True,
+		'tools.staticdir.dir': './js'
+	}
+}
+print(os.getcwd())
+cherrypy.tree.mount (FreeBoard(), '/', conf)
+cherrypy.config.update({'server.socket_host': '0.0.0.0'})
+cherrypy.config.update({'server.socket_port': 8081})
+cherrypy.engine.start()
+cherrypy.engine.block()
